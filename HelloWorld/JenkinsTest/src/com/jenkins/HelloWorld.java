e939eab2b7092b455a2357edefb18089e67bed2e
JunkyardStar <jibbgh@gmail.com>
2018-02-06 15:51:37 -0600

    new change to test jenkins

--- HelloWorld/JenkinsTest/src/com/jenkins/HelloWorld.java
+++ HelloWorld/JenkinsTest/src/com/jenkins/HelloWorld.java
@@ -1,7 +1,10 @@
 package com.jenkins;
 
 public class HelloWorld {
+	
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+		System.out.println("New Change!");
+
 	}
 }
